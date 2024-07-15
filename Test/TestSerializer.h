74609493693d3ce40bced03501a21d8dccb2ec7c
ATRi111 <1657125180@qq.com>
2024-07-15 09:13:07 +0800

    introduce framework

---
+++ Test/TestSerializer.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include<iostream>
+#include<fstream>
+#include<ios>
+#include<string>
+#include"TestSet.h"
+
+class TestSerializer
+{
+protected:
+	virtual void Serialize(std::ofstream& stream, const TestSet& set) const = 0;
+	virtual TestSet Deserialize(std::ifstream& stream) const = 0;
+public:
+	void SerializeTo(std::string path, const TestSet& set) const;
+	TestSet DeserializeFrom(std::string path) const;
+};
+
+std::vector<std::string> Split(std::string s, char seperator);
---
+++ Vector/Vector2Int.h
@@ -0,0 +1,101 @@
+#pragma once
+#include"Vector2.h"
+#include<string>
+#include<vector>
+#include<ctype.h>
+
+inline std::vector<std::string> SplitVector(std::string s)
+{
+	if (!isdigit(s[0]))
+		s = s.substr(1, s.length() - 2);
+	std::vector<std::string> ret;
+	size_t i = s.find_first_of(',');
+	while (i != std::string::npos)
+	{
+		ret.emplace_back(s.substr(0, i));
+		if (i + 1 >= s.length())
+		{
+			s = "";
+			break;
+		}
+		else
+			s = s.substr(i + 1);
+		i = s.find_first_of(',');
+	}
+	if (s != "")
+	{
+		ret.push_back(s);
+	}
+	return ret;
+}
+
+struct Vector2Int
+{
+	static Vector2Int FromString(std::string s)
+	{
+		auto ss = SplitVector(s);
+		int x = std::stoi(ss[0]);
+		int y = std::stoi(ss[1]);
+		return Vector2Int(x, y);
+	}
+
+	const static Vector2Int Zero;
+	const static Vector2Int One;
+	const static Vector2Int Up;
+	const static Vector2Int Down;
+	const static Vector2Int Right;
+	const static Vector2Int Left;
+
+	int x, y;
+	Vector2Int()
+		:x(0), y(0)
+	{
+
+	}
+	Vector2Int(int x, int y)
+		:x(x), y(y)
+	{
+
+	}
+	Vector2Int& operator=(const Vector2Int& v)
+	{
+		x = v.x;
+		y = v.y;
+		return *this;
+	}
+	bool operator==(const Vector2Int& v) const
+	{
+		return x == v.x && y == v.y;
+	}
+	Vector2Int operator+(const Vector2Int& v) const
+	{
+		return Vector2Int(x + v.x, y + v.y);
+	}
+	Vector2Int operator-() const
+	{
+		return Vector2Int(-x, -y);
+	}
+	Vector2Int operator-(const Vector2Int& v) const
+	{
+		return Vector2Int(x - v.x, y - v.y);
+	}
+	Vector2Int operator*(int i) const
+	{
+		return Vector2Int(i * x, i * y);
+	}
+
+	int SqrMagnitude() const
+	{
+		return x * x + y * y;
+	}
+};
+
+inline Vector2Int operator*(int i, const Vector2Int& v)
+{
+	return Vector2Int(i * v.x, i * v.y);
+}
+inline std::ostream& operator<<(std::ostream& stream, const Vector2Int& v)
+{
+	stream << "(" << v.x << "," << v.y << ")";
+	return stream;
+}
